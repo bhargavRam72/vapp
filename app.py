36abd71de865ef34b454cc5c2c24add75267ab8c
bhargavRam72 <bhargav7331@gmail.com>
2026-02-04 13:59:01 -0600

    changed the port to 80

--- app.py
+++ app.py
@@ -57,4 +57,4 @@ def save_reply():                          # Changed function name
 
 if __name__ == "__main__":
     # host='0.0.0.0' makes it accessible publicly
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=80)
